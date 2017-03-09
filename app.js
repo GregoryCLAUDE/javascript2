19e410e14b92cadc8007c4ca8d9b7043569969e4
Grégory <claudejurado@orange.fr>
2017-03-09 17:23:29 +0100

    avance du projet tentative de création d'une boucle

--- app.js
+++ app.js
@@ -4,7 +4,24 @@ console.log ("hello");
 
 var solution = Math.floor(Math.random()*4);
 
-
-
 var response = prompt ("choississez un chiffre entre 0 et 3");
- 
+
+
+	//if (solution < response) 
+	//console.log ("vous êtes au dessus");
+	//else if (response < solution)
+	//	console.log ("vous ëtes au dessous")
+	//else 
+	//	console.log("félicitation vous avez trouver!");
+	
+
+for (solution = 0; solution != response; solution) {
+
+if (solution < response) {
+	console.log ("vous êtes au dessus");}
+	else if (response < solution){
+		console.log ("vous ëtes au dessous");}
+	
+	else 
+		console.log("félicitation vous avez trouvé!");
+	
